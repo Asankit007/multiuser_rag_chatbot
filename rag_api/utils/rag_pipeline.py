33a39aaf3282d116267765c9046bdf1b97c207b9
Ankit Kumar Gupta <147752216+Asankit007@users.noreply.github.com>
2026-05-22 02:42:50 +0530

    Final deployment fix

--- rag_api/utils/rag_pipeline.py
+++ rag_api/utils/rag_pipeline.py
@@ -2,8 +2,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 # from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+# from langchain_openai import OpenAIEmbeddings
+# from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import os
 from langchain_cohere import CohereEmbeddings
 from dotenv import load_dotenv
