20393ce87fc1c701c705501f1a6f37fbfb8dd20c
Ankit Kumar Gupta <147752216+Asankit007@users.noreply.github.com>
2026-05-22 02:36:01 +0530

    Final deployment fix

--- rag_api/utils/rag_pipeline.py
+++ rag_api/utils/rag_pipeline.py
@@ -1,11 +1,34 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+# from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import os
+from langchain_cohere import CohereEmbeddings
+from dotenv import load_dotenv
+load_dotenv()
 
-embedding_model = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
+# embedding_model = HuggingFaceEmbeddings(
+#     model_name="sentence-transformers/all-MiniLM-L6-v2"
+# )
+
+# embedding_model = OpenAIEmbeddings(
+#     api_key=os.getenv("OPENAI_API_KEY")
+# )
+
+# embedding_model = GoogleGenerativeAIEmbeddings(
+#     model="models/embedding-001",
+#     google_api_key=os.getenv("GOOGLE_API_KEY"),
+#     transport="rest"
+# )
+
+embedding_model = CohereEmbeddings(
+    model="embed-english-v3.0",
+    cohere_api_key=os.getenv("COHERE_API_KEY")
 )
+
+
 
 def process_pdf(pdf_path,user_id):
 
